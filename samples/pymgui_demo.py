9e0855df0ebc540286f11fb6c19f01aa1b42505d
sevas <f.degroef@gmail.com>
2016-08-28 21:57:23 +0200

    the no-window demo now uses the stub_render() from pymgui

--- samples/pymgui_demo.py
+++ samples/pymgui_demo.py
@@ -5,18 +5,9 @@ import matplotlib.pyplot as plt
 plt.set_cmap('viridis')
 
 
-def render(draw_data):
-    d = draw_data
-    d.valid
-    print(d.valid)
-    print(d.total_vertex_count)
-    print(d.total_index_count)
-    cmds = d.cmd_lists
-
-
 def main():
     io = pymgui.get_io()
-    io.render_draw_list_func = render
+    io.render_draw_list_func = pymgui.types.stub_render
 
     font_atlas = io.fonts
     font_atlas_texture = pymgui.get_tex_data_as_rgba32(font_atlas)
@@ -27,6 +18,7 @@ def main():
 
     clear_color = pymgui.ImColor(114, 144, 154, 255)
     button_size = pymgui.ImVec2(30,50)
+    show_second_window = 0
 
     while True:
         f = 0.0
@@ -39,6 +31,12 @@ def main():
         pymgui.button("hello button 2", (20, 60))
 
         pymgui.end()
+
+        if show_second_window % 2 == 0:
+            pymgui.begin("hello2", True)
+            pymgui.button("hello", (45, 90))
+            pymgui.end()
+
         pymgui.render()
 
 
